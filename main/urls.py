b7b53288f213e40704d3556070eadeee2308f6fe
Russ Garrett <russ@garrett.co.uk>
2017-01-08 21:32:26 +0000

    Minimum viable website

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^start-a-space$', views.starting, name='starting'),
+    url(r'^login$', views.Login.as_view(), name='login'),
+    url(r'^logout$', views.logout_view, name='logout'),
+]
---
+++ main/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.views import View
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def starting(request):
+    return render(request, 'main/starting.html')
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, 'main/login.html')
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+        else:
+            messages.error(request, "Invalid username or password")
+            return render(request, 'main/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
