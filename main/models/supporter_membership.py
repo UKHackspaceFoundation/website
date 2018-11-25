839f3f3066cd1dc52e6957b534a656a1830e952f
Russ Garrett <russ@garrett.co.uk>
2018-11-25 13:00:05 +0000

    Better error when gocardless credentials aren't configured

--- main/models/supporter_membership.py
+++ main/models/supporter_membership.py
@@ -16,9 +16,12 @@ logger = logging.getLogger(__name__)
 
 # utility functions:
 def get_gocardless_client():
+    if not getattr(settings, "GOCARDLESS_ACCESS_TOKEN", None) or \
+            not getattr(settings, "GOCARDLESS_ENVIRONMENT", None):
+        raise Exception("No GoCardless credentials configured")
     return gocardless_pro.Client(
-        access_token=getattr(settings, "GOCARDLESS_ACCESS_TOKEN", None),
-        environment=getattr(settings, "GOCARDLESS_ENVIRONMENT", None)
+        access_token=settings.GOCARDLESS_ACCESS_TOKEN,
+        environment=settings.GOCARDLESS_ENVIRONMENT
     )
 
 
