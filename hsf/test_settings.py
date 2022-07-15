d2d064812728ea0f6c4a1a9e03302d0adab0947c
Jess Robinson <castaway@desert-island.me.uk>
2022-07-15 12:42:09 +0000

    Add a github actions workflow for CI tests

--- hsf/test_settings.py
+++ hsf/test_settings.py
@@ -12,7 +12,8 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": "",
+        "PASSWORD": "postgres",
+        "HOST":"localhost",
         "PORT": 5432,
     }
 }
